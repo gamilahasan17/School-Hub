8fd18609a6a7ab7775c86c34049a1a6b3b54508b
David-2132005 <d08918125@gmail.com>
2026-09-14 18:51:18 +0300

    ADD Dark&light mode

--- index.js
+++ index.js
@@ -49,3 +49,18 @@ btn.onclick = function () {
 
 };
 
+// Dark / Light Mode
+
+let themeBtn = document.getElementById("themeBtn");
+
+themeBtn.onclick = function () {
+
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        themeBtn.innerHTML = "☀️ Light";
+    } else {
+        themeBtn.innerHTML = "🌙 Dark";
+    }
+
+};
